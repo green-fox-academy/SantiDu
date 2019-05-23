809fc7333ccbe2e762b1ca14aecff6945946804b
SantiDu <santideparis@gmail.com>
2019-05-23 20:31:24 +0800

    day-3

--- week-03/day-3/Query-parameters/main.py
+++ week-03/day-3/Query-parameters/main.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 from form import ProductForm
-
+import csv
 
 
 app = Flask(__name__)
@@ -9,28 +9,41 @@ app.config['SECRET_KEY'] = 'hard to guess string'
 @app.route("/", methods=['GET', 'POST'])
 def webform():
     data = None
+    field = None
     form = ProductForm()
     if form.validate_on_submit():
         if form.name.data:
-            data = form.name.data
+            data = request.form.get("field")
+            field = "name"
         elif form.price.data:
-            data = form.price.data
+            data = request.form.get('field')
+            field = "price"
         else:
-            data = form.qty.data
+            data = request.form.get('field')
+            field = "qty"
+        session["data"] = queryProducts(data, field)
         return redirect(url_for("productinfo"))
     return render_template("webform.html", form=form)
 
 
-def queryProducts(data):
-    with open("products.csv") as csv_file:
+def queryProducts(data, field):
+    with open("./static/products.csv") as csv_file:
         reader = csv.DictReader(csv_file, delimiter=';')
+        output = []
         for row in reader:
-            pass
+            if field in ["qty", "price"]:
+                if row[field] == data:
+                    output.append(row)
+            else:
+                if data in row[field]:
+                    output.append(row)
+        return output
 
 
 @app.route("/productinfo")
 def productinfo():
-    return "abc"
+    output = session["data"]
+    return f"{output}"
 
 
 
